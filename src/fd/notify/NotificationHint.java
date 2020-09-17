daddd92e9faeb8142e4fd59c8f7050dcd76c096f
OopsieWoopsie <xiaosheidy@gmail.com>
2020-09-17 19:10:19 -0300

    aligned hints for better reading

--- src/fd/notify/NotificationHint.java
+++ src/fd/notify/NotificationHint.java
@@ -2,13 +2,13 @@ package fd.notify;
 
 public class NotificationHint<T> {
 
-    public static final NotificationHint<Boolean> ACTION_ICONS = new NotificationHint<>("action-icons");
-    public static final NotificationHint<String> CATEGORY = new NotificationHint<>("category");
-    public static final NotificationHint<String> DESKTOP_ENTRY = new NotificationHint<>("desktop-entry");
-    public static final NotificationHint<String> IMAGE_PATH = new NotificationHint<>("image-path");
-    public static final NotificationHint<Boolean> RESIDENT = new NotificationHint<>("resident");
-    public static final NotificationHint<String> SOUND_FILE = new NotificationHint<>("sound-file");
-    public static final NotificationHint<Byte> URGENCY = new NotificationHint<>("urgency");
+    public static final NotificationHint<Boolean> ACTION_ICONS  = new NotificationHint<>("action-icons");
+    public static final NotificationHint<String>  CATEGORY      = new NotificationHint<>("category");
+    public static final NotificationHint<String>  DESKTOP_ENTRY = new NotificationHint<>("desktop-entry");
+    public static final NotificationHint<String>  IMAGE_PATH    = new NotificationHint<>("image-path");
+    public static final NotificationHint<Boolean> RESIDENT      = new NotificationHint<>("resident");
+    public static final NotificationHint<String>  SOUND_FILE    = new NotificationHint<>("sound-file");
+    public static final NotificationHint<Byte>    URGENCY       = new NotificationHint<>("urgency");
 
     private final String name;
 
