c543d6e768131e897cf6a9cf78bec8f1bee12d81
OopsieWoopsie <xiaosheidy@gmail.com>
2020-09-18 04:48:25 -0300

    added javadoc (texts from freedesktop.org)

--- src/fd/notify/NotificationServer.java
+++ src/fd/notify/NotificationServer.java
@@ -45,14 +45,58 @@ public abstract class NotificationServer implements Notifications, AutoCloseable
 
     private final Quad<String, String, String, String> serverInfo;
 
+    /**
+     * Create a new <code>NotificationServer</code> instance.
+     * 
+     * @param name    The product name of the server.
+     * @param vendor  The vendor name. For example, "KDE," "GNOME,"
+     *                "freedesktop.org," or "Microsoft."
+     * @param version The server's version number.
+     */
     public NotificationServer(String name, String vendor, String version) {
         this.serverInfo = new Quad<String, String, String, String>(name, vendor, version, "1.2");
     }
 
+    /**
+     * Handle a <code>NotificationRequest</code>.<br>
+     * <br>
+     * If <code>req.replacesId()</code> is 0, the return value is a <code>int</code>
+     * that represent the notification. It is unique, and will not be reused unless
+     * a <code>Integer.MAX_VALUE</code> number of notifications have been generated.
+     * An acceptable implementation may just use an incrementing counter for the ID.
+     * The returned ID is always greater than zero. Servers must make sure not to
+     * return zero as an ID.<br>
+     * <br>
+     * If <code>req.replacesId()</code> is not 0, the returned value is the same
+     * value as <code>req.replacesId()</code>.
+     * 
+     * @param req the <code>NotificationRequest</code> to handle.
+     * @return the id of the notification.
+     */
     public abstract int handleNotificaton(NotificationRequest req);
 
+    /**
+     * Cause a notification to be forcefully closed and removed from the user's
+     * view. It can be used, for example, in the event that what the notification
+     * pertains to is no longer relevant, or to cancel a notification with no
+     * expiration time.
+     * 
+     * @param id the id of the notification to be closed.
+     */
     public abstract void closeNotificaton(int id);
 
+    /**
+     * This signal is emitted when one of the following occurs: <br>
+     * &bull; The user performs some global "invoking" action upon a notification. For
+     * instance, clicking somewhere on the notification itself. <br>
+     * &bull; The user invokes a specific action as specified in the original Notify
+     * request. For example, clicking on an action button.
+     * 
+     * @param id         The ID of the notification emitting the ActionInvoked
+     *                   signal.
+     * @param action_key The key of the action invoked. These match the keys sent
+     *                   over in the list of actions.
+     */
     public final void actionInvoked(int id, String action_key) {
         try {
             conn.sendSignal(new ActionInvoked(
@@ -62,6 +106,13 @@ public abstract class NotificationServer implements Notifications, AutoCloseable
         } catch (DBusException e) {/*ignore*/}
     }
 
+    /**
+     * A completed notification is one that has timed out, or has been dismissed by
+     * the user.
+     * 
+     * @param id     The ID of the notification that was closed.
+     * @param reason The reason the notification was closed.
+     */
     public final void notificationClosed(int id, int reason) {
         try {
             conn.sendSignal(new NotificationClosed(
